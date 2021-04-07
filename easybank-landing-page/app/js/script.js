2bbe21666bd71dc1601f0c7bbdc76f886eacfb47
Vitor Bertti <berttivitor@gmail.com>
2021-04-07 20:40:56 -0300

    Created overlay

--- easybank-landing-page/app/js/script.js
+++ easybank-landing-page/app/js/script.js
@@ -1,9 +1,10 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
 
 btnHamburger.addEventListener('click', function() {
-   if(btnHamburger.classList.contains('open')) {
-      btnHamburger.classList.remove('open');
+   if(header.classList.contains('open')) {
+      header.classList.remove('open');
    }else {
-      btnHamburger.classList.add('open');
+      header.classList.add('open');
    }
 });
