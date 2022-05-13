af56ed18af3830d1804071b204828f02ab3a9a67
Vitor Bertti <berttivitor@gmail.com>
2022-05-13 20:50:00 -0300

    Added chooseWord function

--- wordle-clone/app/js/script.js
+++ wordle-clone/app/js/script.js
@@ -3,3 +3,8 @@ let currentGuessCount = 1
 let currentGuess = document.querySelector('#guess' + currentGuessCount)
 let words = ['apple', 'baker', 'store', 'horse', 'speak', 'clone', 'bread']
 let solutionWord = ''
+
+const chooseWord = () => {
+   let randomItem = Math.floor(Math.random() * (words.length - 1)) + 1
+   solutionWord = words[randomItem]
+};
