7551d3a709c4788f4807725fc6c9c8c0780ae579
Vitor Bertti <berttivitor@gmail.com>
2022-05-15 20:59:49 -0300

    Added keydown event listener

--- wordle-clone/app/js/script.js
+++ wordle-clone/app/js/script.js
@@ -7,6 +7,19 @@ let solutionWord = ''
 const chooseWord = () => {
    let randomItem = Math.floor(Math.random() * (words.length - 1)) + 1
    solutionWord = words[randomItem]
-};
+}
 
-chooseWord();
+chooseWord()
+
+document.addEventListener('keydown', (e) => {
+   let keypress = e.key
+   if (currentGuessCount < 7) {
+      if (keypress.length == 1 && lettersPattern.test(e.key) && currentGuess.dataset.letters.length < 5) {
+         updateLetters(keypress)
+      } else if (e.key == 'Backspace' && currentGuess.dataset.letters != '') {
+         deleteFromLetters()
+      } else if (e.key == 'Enter' && currentGuess.dataset.letters.length == 5) {
+         submitGuess()
+      }
+   }
+})
