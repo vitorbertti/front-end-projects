51dc47d56afe6b250b2861f9b83244ec9aba0fb7
Vitor Bertti <berttivitor@gmail.com>
2023-01-17 21:51:13 -0300

    Added nav variables

--- news-homepage/app/js/main.js
+++ news-homepage/app/js/main.js
@@ -1,1 +1,7 @@
-import "./style.css";
+import "./style.css"
+
+const mobileNavBtn = document.querySelector("#mobileNavBtn")
+const mobileNav = document.querySelector("#mobileNav")
+const navLinks = document.querySelectorAll(".nav-link")
+const closeBtn = document.querySelector("#closeBtn")
+const navBkg = document.querySelector(".nav-bkg")
