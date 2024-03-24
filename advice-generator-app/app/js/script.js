65766dd75010b7e72e0c316bb5fd839c76488d77
Vitor Bertti <berttivitor@gmail.com>
2024-03-24 19:35:02 -0300

    Added getUrl function

--- advice-generator-app/app/js/script.js
+++ advice-generator-app/app/js/script.js
@@ -2,3 +2,10 @@ const url = "https://api.adviceslip.com/advice"
 const button = document.querySelector(".dice")
 const adviceID = document.querySelector(".advice")
 const adviceText = document.querySelector(".textAdvice")
+
+async function getUrl() {
+   const response = await fetch(url)
+   const data = await response.json()
+   adviceID.innerHTML = data.slip.id
+   adviceText.innerHTML = data.slip.advice
+}
