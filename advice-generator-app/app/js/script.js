0ef5cc7a60b846a970655f074e6999b8c31e751b
Vitor Bertti <berttivitor@gmail.com>
2024-03-23 21:06:15 -0300

    Added script.js

---
+++ advice-generator-app/app/js/script.js
@@ -0,0 +1,4 @@
+const url = "https://api.adviceslip.com/advice"
+const button = document.querySelector(".dice")
+const adviceID = document.querySelector(".advice")
+const adviceText = document.querySelector(".textAdvice")
