0cfc32ed6efdd4b594046264ddec6a3082567276
Vitor Bertti <berttivitor@gmail.com>
2024-05-05 21:24:56 -0300

    Added click event listener

--- ping-single-column-coming-soon-page/app/js/script.js
+++ ping-single-column-coming-soon-page/app/js/script.js
@@ -1,3 +1,17 @@
 const input = document.querySelector(".input")
 const submit = document.querySelector(".button")
 const error = document.querySelector(".error")
+
+submit.addEventListener("click", function () {
+   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+ 
+   if (!re.test(input.value.toLowerCase())) {
+      error.style.display = "block"
+      input.style.borderColor = "red"
+   } else {
+      input.style.borderColor = "green"
+      error.style.display = "none"
+   }
+ 
+   input.value = ""
+})
