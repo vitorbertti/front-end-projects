c0bb58129f4579a6be33bc2575c5132aa4f4ceec
Vitor Bertti <berttivitor@gmail.com>
2024-06-03 20:02:11 -0300

    Added variables

---
+++ interactive-rating-component/app/js/script.js
@@ -0,0 +1,6 @@
+let button = document.querySelector(".button")
+let ratings = document.querySelectorAll("#circle")
+let number = document.querySelector(".number")
+let endState = document.querySelector(".endState")
+let startState = document.querySelector(".startState")
+let currentClickedElement = null
