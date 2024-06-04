b59112a7efddb1ec7a5cdf2064f745e14df6c111
Vitor Bertti <berttivitor@gmail.com>
2024-06-04 20:15:17 -0300

    Added click event listener

--- interactive-rating-component/app/js/script.js
+++ interactive-rating-component/app/js/script.js
@@ -4,3 +4,21 @@ let number = document.querySelector(".number")
 let endState = document.querySelector(".endState")
 let startState = document.querySelector(".startState")
 let currentClickedElement = null
+
+ratings.forEach((element) => {
+   element.addEventListener('click' , () => {
+      if(currentClickedElement){
+         currentClickedElement.style.backgroundColor = ""
+      }
+
+      element.style.backgroundColor ="#979797"
+      element.style.color ="white"
+      currentClickedElement =element
+
+      button.addEventListener('click', function(){
+         endState.style.display = "flex"
+         startState.style.display = "none"
+         number.innerHTML = element.innerHTML  
+      })
+   })
+})
