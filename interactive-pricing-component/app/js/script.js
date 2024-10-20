c37d879a48e356a2e6fd4307b8a476fb027da802
Vitor Bertti <berttivitor@gmail.com>
2024-10-20 19:36:14 -0300

    Added data variable

--- interactive-pricing-component/app/js/script.js
+++ interactive-pricing-component/app/js/script.js
@@ -4,3 +4,31 @@ const rangeInput = document.querySelector(".myrange")
 const views = document.querySelector(".number")
 const cost = document.querySelector(".amount")
 const discount = document.querySelector(".discountNumber")
+
+const data = [
+   {
+      pageviews: "10k",
+      monthlycost: 8,
+      leftpercentage: 0,
+   },
+   {
+      pageviews: "50k",
+      monthlycost: 12,
+      leftpercentage: 25,
+   },
+   {
+      pageviews: "100k",
+      monthlycost: 16,
+      leftpercentage: 50,
+   },
+   {
+      pageviews: "500k",
+      monthlycost: 24,
+      leftpercentage: 75,
+   },
+   {
+      pageviews: "1M",
+      monthlycost: 36,
+      leftpercentage: 100,
+   },
+]
