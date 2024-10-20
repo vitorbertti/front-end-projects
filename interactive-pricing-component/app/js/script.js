5be2b941ec8e9a3519330dcca0e02ad447e0bce1
Vitor Bertti <berttivitor@gmail.com>
2024-10-19 21:01:36 -0300

    Added variables

---
+++ interactive-pricing-component/app/js/script.js
@@ -0,0 +1,6 @@
+'use strict'
+
+const rangeInput = document.querySelector(".myrange")
+const views = document.querySelector(".number")
+const cost = document.querySelector(".amount")
+const discount = document.querySelector(".discountNumber")
