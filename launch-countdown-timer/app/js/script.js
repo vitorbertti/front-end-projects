2f40a359e90361d42c837438398862ab142bab15
Vitor Bertti <berttivitor@gmail.com>
2025-03-05 19:27:22 -0300

    Added today variable

--- launch-countdown-timer/app/js/script.js
+++ launch-countdown-timer/app/js/script.js
@@ -1,1 +1,5 @@
-const countdownDate = new Date("Mar 31, 2025 23:52:00").getTime();
+const countdownDate = new Date("Mar 31, 2025 23:52:00").getTime()
+
+let x = setInterval(() => {
+   const today = new Date().getTime()
+ }, 1000)
