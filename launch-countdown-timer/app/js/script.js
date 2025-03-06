6ab49e0858afc64f239d1a4cc4e1a2712dfd9b55
Vitor Bertti <berttivitor@gmail.com>
2025-03-06 19:52:12 -0300

    Added distance variable

--- launch-countdown-timer/app/js/script.js
+++ launch-countdown-timer/app/js/script.js
@@ -2,4 +2,5 @@ const countdownDate = new Date("Mar 31, 2025 23:52:00").getTime()
 
 let x = setInterval(() => {
    const today = new Date().getTime()
+   let distance = countdownDate - today
  }, 1000)
