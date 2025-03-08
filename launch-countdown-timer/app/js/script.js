0adfe4143afc43def1d4bc6833eadbce815383e7
Vitor Bertti <berttivitor@gmail.com>
2025-03-07 23:35:26 -0300

    Added days variable

--- launch-countdown-timer/app/js/script.js
+++ launch-countdown-timer/app/js/script.js
@@ -1,6 +1,9 @@
 const countdownDate = new Date("Mar 31, 2025 23:52:00").getTime()
 
 let x = setInterval(() => {
-   const today = new Date().getTime()
-   let distance = countdownDate - today
+  const today = new Date().getTime()
+  let distance = countdownDate - today
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24))
+
+  document.getElementById("days").innerHTML = days
  }, 1000)
