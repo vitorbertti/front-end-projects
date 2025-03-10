35fddcefa98b4a178cc79e4217076cc38c7a2321
Vitor Bertti <berttivitor@gmail.com>
2025-03-10 19:04:27 -0300

    Added seconds variable

--- launch-countdown-timer/app/js/script.js
+++ launch-countdown-timer/app/js/script.js
@@ -6,8 +6,10 @@ let x = setInterval(() => {
   let days = Math.floor(distance / (1000 * 60 * 60 * 24))
   let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
   let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000)
 
   document.getElementById("days").innerHTML = days
   document.getElementById("hours").innerHTML = hours
   document.getElementById("minutes").innerHTML = minutes
+  document.getElementById("seconds").innerHTML = seconds
  }, 1000)
