e9f27c2863850a1e90af9a3022921ca4ac60d7f4
Vitor Bertti <berttivitor@gmail.com>
2025-04-16 19:26:48 -0300

    Added input variables

--- age-calculator-app/app/js/script.js
+++ age-calculator-app/app/js/script.js
@@ -1,1 +1,5 @@
 let button = document.querySelector(".button")
+
+let day = document.querySelector(".dayInput")
+let month = document.querySelector(".monthInput")
+let year = document.querySelector(".yearInput")
