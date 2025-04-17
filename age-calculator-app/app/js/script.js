86f1b386be5e0d43fe0be1958c439b54fefd54f3
Vitor Bertti <berttivitor@gmail.com>
2025-04-17 19:33:08 -0300

    Added number variables

--- age-calculator-app/app/js/script.js
+++ age-calculator-app/app/js/script.js
@@ -3,3 +3,7 @@ let button = document.querySelector(".button")
 let day = document.querySelector(".dayInput")
 let month = document.querySelector(".monthInput")
 let year = document.querySelector(".yearInput")
+
+let yearNumber = document.querySelector(".yearNumber")
+let monthNumber = document.querySelector(".monthNumber")
+let dayNumber = document.querySelector(".dayNumber")
