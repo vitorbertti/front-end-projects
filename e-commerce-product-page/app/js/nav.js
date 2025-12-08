169c98341e5d83cefe693bba04cc6b0b554b0e4f
Vitor Bertti <berttivitor@gmail.com>
2025-12-07 23:46:41 -0300

    Added nav.js toggleMenu function

--- e-commerce-product-page/app/js/nav.js
+++ e-commerce-product-page/app/js/nav.js
@@ -1,3 +1,7 @@
 const closeMenuBtn = document.querySelector('#closeMenu')
 const openMenuBtn = document.querySelector('#openMenu')
 const navPanelContainer = document.querySelector('.nav__panel-container')
+
+function toggleMenu(){
+   navPanelContainer.classList.toggle('expanded')
+}
