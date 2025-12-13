2c9287dcdf6ad8f045e40f5798023cb35ebb88e9
Vitor Bertti <berttivitor@gmail.com>
2025-12-13 14:10:22 -0300

    Added slider.js moveSlider function

--- e-commerce-product-page/app/js/slider.js
+++ e-commerce-product-page/app/js/slider.js
@@ -3,3 +3,20 @@ const sliderContainer = document.querySelector('.slider-container')
 const slider = document.querySelector('.slider')
 const slide = [...document.querySelectorAll('.slide')]
 let currentIndex = 0
+
+function moveSlider(dir){
+   dir === 'next' ? currentIndex++ : currentIndex --
+   slider.style.transform = `translateX(-${100 * currentIndex}%)`
+
+   switch (currentIndex){
+      case 0:
+         document.querySelector('#prev').classList.add('hidden')
+         break
+      case slide.length - 1:
+         document.querySelector('#next').classList.add('hidden')
+         break
+      default: 
+         btns.forEach(b => b.classList.remove('hidden'))
+      break
+   }
+}
