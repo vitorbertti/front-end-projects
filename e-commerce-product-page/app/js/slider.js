6478f413fc26b8e2e94d702f8cf7447758666876
Vitor Bertti <berttivitor@gmail.com>
2025-12-12 23:41:24 -0300

    Added slider.js variables

---
+++ e-commerce-product-page/app/js/slider.js
@@ -0,0 +1,5 @@
+const btns = document.querySelectorAll('.btn')
+const sliderContainer = document.querySelector('.slider-container')
+const slider = document.querySelector('.slider')
+const slide = [...document.querySelectorAll('.slide')]
+let currentIndex = 0
