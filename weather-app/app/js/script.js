7df4168f72747df62490d4a4fc2505459600a72d
Vitor Bertti <berttivitor@gmail.com>
2026-03-30 19:44:09 -0300

    Added js variables

---
+++ weather-app/app/js/script.js
@@ -0,0 +1,13 @@
+const ddlUnits = document.querySelector("#ddlUnits");
+const ddlDay = document.querySelector("#ddlDay");
+const txtSearch = document.querySelector("#txtSearch");
+const btnSearch = document.querySelector("#btnSearch");
+const dvCityCountry = document.querySelector("#dvCityCountry");
+const dvCurrDate = document.querySelector("#dvCurrDate");
+const dvCurrTemp = document.querySelector("#dvCurrTemp");
+const pFeelsLike = document.querySelector("#pFeelsLike");
+const pHumidity = document.querySelector("#pHumidity");
+const pWind = document.querySelector("#pWind");
+const pPrecipitation = document.querySelector("#pPrecipitation");
+let cityName, countryName, weatherData;
+
