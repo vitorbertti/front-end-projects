946f8e362d4c21b0191356b5ea71393746bcbf26
Vitor Bertti <berttivitor@gmail.com>
2026-04-07 20:04:31 -0300

    Added getWeatherCodeName function

--- weather-app/app/js/script.js
+++ weather-app/app/js/script.js
@@ -160,3 +160,38 @@ function loadHourlyForecast() {
     id++
   }
 }
+
+function getWeatherCodeName(code) {
+  const weatherCodes = {
+    0: "sunny",
+    1: "partly-cloudy",
+    2: "partly-cloudy",
+    3: "overcast",
+    45: "fog",
+    48: "fog",
+    51: "drizzle",
+    53: "drizzle",
+    55: "drizzle",
+    56: "drizzle",
+    57: "drizzle",
+    61: "rain",
+    63: "rain",
+    65: "rain",
+    66: "rain",
+    67: "rain",
+    80: "rain",
+    81: "rain",
+    82: "rain",
+    71: "snow",
+    73: "snow",
+    75: "snow",
+    77: "snow",
+    85: "snow",
+    86: "snow",
+    95: "storm",
+    96: "storm",
+    99: "storm",
+  }
+
+  return weatherCodes[code]
+}
